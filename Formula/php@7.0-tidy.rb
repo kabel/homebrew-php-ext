cc2a31e975d607d4a4c28ef4e801370b30c1f308
Kevin Abel <kevin.abel.0@gmail.com>
2018-04-25 19:51:32 -0500

    Use correct tidy-html5 header
    Fixes #2

--- Formula/php@7.0-tidy.rb
+++ Formula/php@7.0-tidy.rb
@@ -100,4 +100,10 @@ class PhpAT70Tidy < PhpExtensionFormula
   depends_on "tidy-html5"
 
   configure_arg "--with-tidy=#{Formula["tidy-html5"].opt_prefix}"
+
+  def install
+    # API compatibility with tidy-html5 v5.0.0 - https://github.com/htacg/tidy-html5/issues/224
+    inreplace "ext/#{extension}/tidy.c", "buffio.h", "tidybuffio.h"
+    super
+  end
 end
