27d7a1a7539624dfec708602199db7236e9582f9
Doug Stevenson <doug.stevenson@pgi.com>
2018-11-10 13:15:32 -0800

    feature() PHP 7.2 tidy

---
+++ Formula/php@7.2-tidy.rb
@@ -0,0 +1,9 @@
+require File.expand_path("../lib/php_extension_formula", __dir__)
+
+class PhpAT72Tidy < PhpExtensionFormula
+  extension_dsl "Tidy Extension"
+
+  depends_on "tidy-html5"
+
+  configure_arg "--with-tidy=#{Formula["tidy-html5"].opt_prefix}"
+end
