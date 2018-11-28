7caf96932aec16c8f686ffd7d97815236f3e1c86
Kevin Abel <kevin.abel.0@gmail.com>
2018-11-27 21:40:20 -0600

    Remove tidy extensions integrated into homebrew-core.
    See https://github.com/Homebrew/homebrew-core/pull/34496

--- Formula/php@7.1-tidy.rb
+++
@@ -1,9 +0,0 @@
-require File.expand_path("../lib/php_extension_formula", __dir__)
-
-class PhpAT71Tidy < PhpExtensionFormula
-  extension_dsl "Tidy Extension"
-
-  depends_on "tidy-html5"
-
-  configure_arg "--with-tidy=#{Formula["tidy-html5"].opt_prefix}"
-end
