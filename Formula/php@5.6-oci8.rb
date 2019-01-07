0f262fcb419078b7f45241c1553e06d2a5dfcadb
Kevin Abel <kevin.abel.0@gmail.com>
2019-01-07 12:34:40 -0600

    EOL php@5.6

--- Formula/php@5.6-oci8.rb
+++
@@ -1,6 +0,0 @@
-require File.expand_path("../lib/oracle_php_extension_formula", __dir__)
-
-class PhpAT56Oci8 < OraclePhpExtensionFormula
-  extension_dsl "OCI8 Extension"
-  instantclient_options :arg => "--with-oci8"
-end
--- Formula/php@5.6-pdo-oci.rb
+++
@@ -1,6 +0,0 @@
-require File.expand_path("../lib/oracle_php_extension_formula", __dir__)
-
-class PhpAT56PdoOci < OraclePhpExtensionFormula
-  extension_dsl "PDO Driver OCI"
-  instantclient_options :arg => "--with-pdo-oci", :with_version => true
-end
