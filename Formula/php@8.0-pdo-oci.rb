a7b51573faa5953fdd895aa98666ad211a14ac70
Kevin Abel <kevin.abel.0@gmail.com>
2021-11-29 16:02:42 -0600

    Branch for php 8.1 and 8.0 versioned

---
+++ Formula/php@8.0-pdo-oci.rb
@@ -0,0 +1,6 @@
+require_relative "../lib/oracle_php_extension_formula"
+
+class PhpAT80PdoOci < OraclePhpExtensionFormula
+  extension_dsl "PDO Driver OCI"
+  instantclient_options arg: "--with-pdo-oci", with_version: true
+end
---
+++ Formula/php@8.0-recode.rb
@@ -0,0 +1,11 @@
+require_relative "../lib/php_extension_formula"
+
+class PhpAT80Recode < PhpExtensionFormula
+  extension_dsl "GNU Recode Extension"
+
+  conflicts_with "php-imap", because: "because both share the same internal symbols"
+
+  depends_on "recode"
+
+  configure_arg "--with-recode=#{Formula["recode"].opt_prefix}"
+end
