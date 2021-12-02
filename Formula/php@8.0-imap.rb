650a024f7cd8b7d53b101b65d593d86563e6c490
Kevin Abel <kevin.abel.0@gmail.com>
2021-12-02 17:39:09 -0600

    Fix versioned conflicts

--- Formula/php@8.0-imap.rb
+++ Formula/php@8.0-imap.rb
@@ -3,7 +3,7 @@ require_relative "../lib/php_extension_formula"
 class PhpAT80Imap < PhpExtensionFormula
   extension_dsl "IMAP Extension"
 
-  conflicts_with "php-recode", because: "because both share the same internal symbols"
+  conflicts_with "php@8.0-recode", because: "because both share the same internal symbols"
 
   depends_on "imap-uw"
   depends_on "openssl@1.1"
