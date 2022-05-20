8a89cb44e5a7a507e4d35fe67cdca59b8f7c377c
Kevin Abel <kevin.abel.0@gmail.com>
2022-05-20 18:35:32 -0500

    php-enchant: All versions are compatible with upstream enchant dependency

--- Formula/php@8.0-enchant.rb
+++ Formula/php@8.0-enchant.rb
@@ -3,27 +3,5 @@ require_relative "../lib/php_extension_formula"
 class PhpAT80Enchant < PhpExtensionFormula
   extension_dsl "Enchant Extension"
 
-  depends_on "pkg-config" => :build
-  depends_on "aspell"
-  depends_on "gettext"
-  depends_on "glib"
-
-  resource "enchant" do
-    url "https://www.abisource.com/downloads/enchant/1.6.0/enchant-1.6.0.tar.gz"
-    sha256 "2fac9e7be7e9424b2c5570d8affe568db39f7572c10ed48d4e13cddf03f7097f"
-  end
-
-  def install
-    configure_args << "--with-enchant=#{prefix}/vendor"
-
-    resource("enchant").stage do
-      system "./configure", "--disable-dependency-tracking",
-                            "--prefix=#{prefix}/vendor",
-                            "--disable-ispell",
-                            "--disable-myspell"
-
-      system "make", "install"
-    end
-    super
-  end
+  depends_on "enchant"
 end
