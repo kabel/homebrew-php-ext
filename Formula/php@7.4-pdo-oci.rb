018548392d62d49d90eb1799f7a320fc67412e12
Kevin Abel <kevin.abel.0@gmail.com>
2022-12-03 13:56:50 -0600

    End-of-life php@7.4

--- Formula/php@7.4-pdo-oci.rb
+++ Formula/php@7.4-pdo-oci.rb
@@ -2,5 +2,8 @@ require_relative "../lib/oracle_php_extension_formula"
 
 class PhpAT74PdoOci < OraclePhpExtensionFormula
   extension_dsl "PDO Driver OCI"
+
+  disable! date: "2022-11-28", because: :versioned_formula
+
   instantclient_options arg: "--with-pdo-oci", with_version: true
 end
--- Formula/php@7.4-snmp.rb
+++ Formula/php@7.4-snmp.rb
@@ -3,6 +3,8 @@ require_relative "../lib/php_extension_formula"
 class PhpAT74Snmp < PhpExtensionFormula
   extension_dsl "SNMP Extension"
 
+  disable! date: "2022-11-28", because: :versioned_formula
+
   depends_on "net-snmp"
   depends_on "openssl@1.1"
 
