0af0c5f1a744a341b80de195fb584825e09c69f7
Kevin Abel <kevin.abel.0@gmail.com>
2022-12-11 11:24:24 -0600

    php@7.3 removed from homebrew-core
    End-of-life

--- Formula/php@7.3-pdo-oci.rb
+++
@@ -1,9 +0,0 @@
-require_relative "../lib/oracle_php_extension_formula"
-
-class PhpAT73PdoOci < OraclePhpExtensionFormula
-  extension_dsl "PDO Driver OCI"
-
-  disable! date: "2021-12-06", because: :versioned_formula
-
-  instantclient_options arg: "--with-pdo-oci", with_version: true
-end
