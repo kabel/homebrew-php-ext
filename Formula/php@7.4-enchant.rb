bb39f31dec3c8041d098267ec1fa1ab1f4d3ee38
Kevin Abel <kevin.abel.0@gmail.com>
2023-11-27 15:48:02 -0600

    Drop PHP 7.4 for EOL

--- Formula/php@7.4-enchant.rb
+++
@@ -1,9 +0,0 @@
-require_relative "../lib/php_extension_formula"
-
-class PhpAT74Enchant < PhpExtensionFormula
-  extension_dsl "Enchant Extension"
-
-  disable! date: "2022-11-28", because: :versioned_formula
-
-  depends_on "enchant"
-end
--- Formula/php@7.4-oci8.rb
+++
@@ -1,9 +0,0 @@
-require_relative "../lib/oracle_php_extension_formula"
-
-class PhpAT74Oci8 < OraclePhpExtensionFormula
-  extension_dsl "OCI8 Extension"
-
-  disable! date: "2022-11-28", because: :versioned_formula
-
-  instantclient_options arg: "--with-oci8"
-end
