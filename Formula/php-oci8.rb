be44bf9bafe33919035e86a2e49446850a6a8f5e
Kevin Abel <kevin.abel.0@gmail.com>
2024-11-26 17:17:18 -0600

    Remove PHP 8.4 extensions removed from upstream core

--- Formula/php-oci8.rb
+++
@@ -1,6 +0,0 @@
-require_relative "../lib/oracle_php_extension_formula"
-
-class PhpOci8 < OraclePhpExtensionFormula
-  extension_dsl "OCI8 Extension"
-  instantclient_options arg: "--with-oci8"
-end
--- Formula/php-pdo-oci.rb
+++
@@ -1,6 +0,0 @@
-require_relative "../lib/oracle_php_extension_formula"
-
-class PhpPdoOci < OraclePhpExtensionFormula
-  extension_dsl "PDO Driver OCI"
-  instantclient_options arg: "--with-pdo-oci", with_version: true
-end
