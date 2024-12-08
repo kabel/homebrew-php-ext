6f1beba58200ca5652a5507391b30e4ba90ed8e3
Kevin Abel <kevin.abel.0@gmail.com>
2024-12-08 17:08:19 -0600

    Remove php@8.0 for EOL

--- Formula/php@8.0-enchant.rb
+++
@@ -1,9 +0,0 @@
-require_relative "../lib/php_extension_formula"
-
-class PhpAT80Enchant < PhpExtensionFormula
-  extension_dsl "Enchant Extension"
-
-  deprecate! date: "2022-11-26", because: :versioned_formula
-
-  depends_on "enchant"
-end
--- Formula/php@8.0-pdo-oci.rb
+++
@@ -1,9 +0,0 @@
-require_relative "../lib/oracle_php_extension_formula"
-
-class PhpAT80PdoOci < OraclePhpExtensionFormula
-  extension_dsl "PDO Driver OCI"
-
-  deprecate! date: "2022-11-26", because: :versioned_formula
-
-  instantclient_options arg: "--with-pdo-oci", with_version: true
-end
